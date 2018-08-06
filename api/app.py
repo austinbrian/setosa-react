50bbb46584d119fa6e04970f2754f91a6dee1ec0
Brian Austin <austin.brian@gmail.com>
2018-08-06 19:41:17 -0400

    basic iris model api

--- api/app.py
+++ api/app.py
@@ -1,12 +1,42 @@
 #!bin/bash/python3
 
 from flask import Flask
-
+import json
+import os.path
+import sys
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from model.model import get_model
 app = Flask(__name__)
 
+def do_calculation():
+    model_used = 'rf'
+    model, test_val = get_model([.2,1,.1,1],model_used)
+    prediction_number = model.predict(test_val)
+    probabilities = model.predict_proba(test_val)
+    pred_label_dict = {0:'Setosa',1:'Versicolor',2:'Virginica'}
+    pred_label = pred_label_dict[prediction_number[0]]
+    return pred_label, probabilities, model_used
+
+
 @app.route('/')
-def index():
-    return "Flask is running!"
+def hello_world():
+    pred_label, probabilities, model_used = do_calculation()
+    header ="<h1>Estimates...</h1><br>"
+    model = f"<h2>{(model_used).upper()}</h2>"
+    body = f"<h3>{pred_label}</h3><p>{probabilities[0]}<p>"
+    return header+model+body
+
+
+@app.route('/api')
+def get_val():
+    pred_label, probabilities, model_used = do_calculation()
+    # prediction = 'setosa'
+    # vals = [.5, .2, .3]
+    flowers = ["setosa","versicolor","virginica"]
+    json_out = dict(zip(flowers,list(probabilities[0])))
+    json_out['model_used'] = model_used
+    return json.dumps(json_out)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model/model.py
+++ model/model.py
@@ -14,8 +14,8 @@ def get_data():
     iris_y = iris.target
     return iris_X, iris_y
 
-def main(test_val, method='knn'):
-    X, y = get_data() 
+def get_model(test_val, method='knn'):
+    X, y = get_data()
     if method == "knn":
         model = KNeighborsClassifier()
         model.fit(X,y)
@@ -24,16 +24,15 @@ def main(test_val, method='knn'):
         model.fit(X, y)
     elif method =="lr":
         model = LR()
-    test_val = [float(i) for i in test_val.strip('[]').split(',')]
+    test_val = [float(i) for i in str(test_val).strip('[]').split(',')]
     test_val = np.array(test_val).reshape(1, -1)
     return model, test_val
 
 if __name__=="__main__":
-    model, test_val = main(method=sys.argv[1], test_val=sys.argv[2])
+    model, test_val = get_model(method=sys.argv[1], test_val=sys.argv[2])
     prediction_number = model.predict(test_val)
     probabilities = model.predict_proba(test_val)
     pred_label_dict = {0:'Setosa',1:'Versicolor',2:'Virginica'}
     pred_label = pred_label_dict[prediction_number[0]]
     print("Prediction: {}".format(pred_label))
-    print("Probabiliites: {}".format(probabilities))
-
+    print("Probabiliites: {}".format(probabilities[0]))
